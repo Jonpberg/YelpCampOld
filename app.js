1f54a31861c5b785ad451285afc0c22bc572557e
Jon Berg <jonpberg9@gmail.com>
2018-04-27 11:50:32 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ var commentRoutes    = require('./routes/comments'),
     campgroundRoutes = require('./routes/campgrounds'),
     indexRoutes      = require('./routes/index');
 
-// mongoose.connect('mongodb://localhost/yelp_camp');
+
+mongoose.connect(process.env.DATABASEURL);
 mongoose.connect('mongodb://jon:Sponge157018@ds259109.mlab.com:59109/yelpcamp');
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
